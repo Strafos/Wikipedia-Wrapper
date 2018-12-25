7f125633a41f0e83208242b46dcbd687cc91d7ab
Zaibo Wang <zaibowang@gmail.com>
2018-12-25 13:59:55 -0500

    Add sqlite3 interface

--- scrapper.py
+++ scrapper.py
@@ -1,8 +1,33 @@
 import wikipedia
+from pprint import pprint
+import re
+import sqlite3
 
 
-def get_wiki_page(page_name):
-    print(wikipedia.summary("Wikipedia"))
+def filter_lines(lines):
+    """Filter for lines that can be typed on a qwerty keyboard"""
+    pattern = r'^[a-zA-Z0-9.!@?#"$%&:\';()*\+,\/;\-=[\\\]\^_{|}<>~` ]+$'
+    return [line for line in lines if not re.match(pattern, line) and len(line) > 100]
 
 
-get_wiki_page("foo")
+def get_page(page_name):
+    page = wikipedia.page(page_name)
+    title = page.title
+    content = page.content
+    paragraphs = filter_lines(content.split('\n'))
+    return (title, paragraphs)
+
+
+page_title, page_content = get_page("Physics")
+conn = sqlite3.connect('/home/zaibo/code/type/db/typetext.db')
+c = conn.cursor()
+
+c.execute("INSERT INTO pages VALUES(null, ?)", (page_title,))
+
+titled_content = [(page_title, content) for content in page_content]
+c.executemany('INSERT INTO content VALUES (null, ?, ?)', titled_content)
+
+conn.commit()
+conn.close()
+
+# print(filter_lines(["hello", "damn.", "%", "foo!"]))
